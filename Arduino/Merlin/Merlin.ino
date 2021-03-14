d79cde28d141a33c083567cab752a145ec360fe8
Marcelo <artilrobotics@gmail.com>
2021-03-13 21:20:58 -0500

    probando uduino

--- Arduino/Merlin/Merlin.ino
+++ Arduino/Merlin/Merlin.ino
@@ -1,57 +1,89 @@
-#include <SoftwareSerial.h>  //Cambiamos RX pin 9 TX pin 10 del bluetooth
-SoftwareSerial BT(9, 10);     //Para que no interfieran en la carga del sketch con el USB
+#include "Uduino.h"  
 
-int selecrc = 10;
-int selecau = 11;
+//#undef UDUINO_HARDWAREONLY
+//#define UDUINO_HARDWAREONLY 0
+#include <SoftwareSerial.h>
+SoftwareSerial BT(9,10);
+Uduino uduino((9,10),"Merlin");
+
+#define Led1 11
+#define Led2 12
 
 int estado;
+String str = ""; //Valores de labview
+const char separator = '/'; //simbolo para separar
+const int dataLength = 18; //datos que estan llegando
+String data[dataLength]; //almacenando los datos
 
 void setup() {
   BT.begin(9600);      //Comunicacion serial para el bluetooth
-  Serial.begin(115200);  //Comunicacion serial para el monitor serial
+  Serial.begin(9600); 
+  UduinoSetUP();
+  //BT.begin(115200);  //Comunicacion serial para el monitor serial
   //Configuramos los pines de salida:
+  pinMode(Led1, OUTPUT);
+  pinMode(Led2, OUTPUT);
   setupMotores();
   // Velocidad de los motores al iniciar
   velmotor(0, 0);
 }
 
 void loop() {
-  //Bluetooth
-  //if(BT.available() > 0){
-  //estado = BT.read();
-  //Serial
-  Recive();
-
-  //  if (Serial.available() > 0) {
-  //    estado = Serial.read();
+  BT.println(0);
+  uduino.update();
+  uduino.println("uduino");
+  delay(100);
+  if (uduino.isConnected())
+  {
+      uduino.println("conectadoooooo");
+      digitalWrite(Led1, HIGH);
+      delay(500);
+  }
+      digitalWrite(Led2, HIGH);
+      delay(500);
+      digitalWrite(Led2, LOW);
+      delay(500);
+  //  if (BT.available() > 0) {
+  //    estado = BT.read();
   //
-  //    //Serial.println(estado);
+  //    //BT.println(estado);
   //  }
-  Recive();
-  // AVANCE:
-  if (estado == 'F') {
-    velmotor(100, 100);
-    Serial.println("Avance");
-  }
-  // IZQUIERDA
-  else if (estado == 'L') {
-    velmotor(-100, 100);
-    Serial.println("Izq");
-  }
-  // PARADO
-  else if (estado == 'S' ) {
-    velmotor(0, 0);
-    freno(true, true);
-    Serial.println("Parar");
-  }
-  // DERECHA
-  else if (estado == 'R') {
-    velmotor(100, -100);
-    Serial.println("Der");
-  }
-  // REVERSA
-  else if (estado == 'B') {
-    velmotor(-100, -100);
-    Serial.println("Reversa");
-  }
+//  estado=data[0].toInt();
+//  
+//  // ENCENDIDO
+//  if (estado == 1) {
+//    digitalWrite(Led1, HIGH);
+//    delay(500);
+//  }
+//  // APAGADO
+//  if (estado == 0) {
+//    digitalWrite(Led1, LOW);
+//    delay(500);
+//  }
+//  // AVANCE:
+//  if (estado == 'F') {
+//    velmotor(100, 100);
+//    BT.println("Avance");
+//  }
+//  // IZQUIERDA
+//  else if (estado == 'L') {
+//    velmotor(-100, 100);
+//    BT.println("Izq");
+//  }
+//  // PARADO
+//  else if (estado == 'S' ) {
+//    velmotor(0, 0);
+//    freno(true, true);
+//    BT.println("Parar");
+//  }
+//  // DERECHA
+//  else if (estado == 'R') {
+//    velmotor(100, -100);
+//    BT.println("Der");
+//  }
+//  // REVERSA
+//  else if (estado == 'B') {
+//    velmotor(-100, -100);
+//    BT.println("Reversa");
+//  }
 }
